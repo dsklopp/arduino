aae0a0fe4e421735ca541c5e8a531d1c6c219eaf
dsklopp <dsklopp@gmail.com>
2013-01-06 17:12:45 -0500

    Added LCD portion, updated input code to gracefully handle 0 or negative values, new time duration immediately breaks current loop.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 arduino
 =======
 
-Arduino related
+The best means to clone this repository is within your sketchbook directory.  From there edit, add or remove directories in sketchbook/arduino.  Anything else in sketchbook should be considered private and distinct.
+
+Please add contributed Arduino projects to this directory with an appropriate README file in each.  Thanks.
+
+-Dan Klopp
--- shutter_control/shutter_control.ino
+++ shutter_control/shutter_control.ino
@@ -3,30 +3,44 @@
   Author: Dan Klopp (dsklopp@gmail.com)
   Please add youself to the authors list as you contribute code
   
-  WARNING: This code has not been tested yet, waiting for other code
-  contributions before testing.
+  Code is updateable within intervals over a serial connection
+  connect likeso: screen /dev/ttyACM0 9600
+  If a new time is given, the current delay is aborted, a photo taken,
+  and the loop restarted.
  */
  
+#include <LiquidCrystal.h> 
+
 // Change led to the correct pin number when the device arrives
 int led = 13; // currently the led
+LiquidCrystal lcd(7,8,9,10,11,12);
+int seconds = 5*1; // default shutter time
 
 void setup() {                
-  Serial.begin(9600)
-  pinMode(led, OUTPUT);     
+  Serial.begin(9600);
+  lcd.begin(16,1);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
-  //First read input if available
-  int seconds = 5*60;
-  while (Serial.available() > 0) {
-    seconds = Serial.parseInt();
-    //Flushing rest of line
-    while(Serial.read() != -1)
-  }
-  
+    lcd.clear();
+  lcd.print(seconds);
+  lcd.print(" seconds");
   digitalWrite(led, HIGH);
   delay(100);                // Flash shutter for 100 msec
   digitalWrite(led, LOW);
-  delay(seconds*1000);               // Delay in seconds
-  
+
+  int iter=0;
+  if (seconds < 1) { seconds=1; }
+  while (iter < seconds) {
+    iter++;
+    if (Serial.available() > 0) {
+      seconds = Serial.parseInt();
+      //Flushing rest of line
+      while(Serial.read() != -1) {}
+      break;
+    }
+    delay(1000);
+  }
+
 }
